fe58f6c66aafc0b6313af8f44668e21725c7998d
Ryan <ryan.merrill2004@yahoo.com>
2026-05-08 22:29:03 -0400

    wording change to section

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ function App() {
 
                         <div className="card">
                             <h3>Lead With Confidence</h3>
-                            <p>Control attitude, effort, discipline, and communication.</p>
+                            <p>Control attitude, effort, and discipline.</p>
                         </div>
                     </div>
                 </section>
